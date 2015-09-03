1da3961e81b193a31577fae8ffbd7cf3abac5f27
Aaron Todd <atodd@leightronix.com>
2015-09-03 16:13:33 -0400

    cron check

--- src/HelloWorld/SayHello.php
+++ src/HelloWorld/SayHello.php
@@ -6,6 +6,6 @@ class SayHello
 {
     public static function world()
     {
-        return 'Hello World, Composer!\nYou Rock!\n';
+        return 'Hello World, Composer!\r\nCRON worked!!';
     }
 }
